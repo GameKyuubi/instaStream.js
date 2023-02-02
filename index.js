7707a935aef57eae1b899e402a8f33dcbfe56fa6
GameKyuubi <xtra.zero@gmail.com>
2023-02-01 22:18:39 -0800

    add node-config, default config, boilerplate

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 const OBSWebSocket = require('obs-websocket-js').default;
+const config = require('config');
+const owsConfig = config.get('obs-websocket');
 
 const obs = new OBSWebSocket();
 console.log("hello from JS");
+obs.connect(owsConfig.url, owsConfig.password, identificationParams = {}).then((data) => {
+  console.log(data);
+  obs.disconnect();
+});
